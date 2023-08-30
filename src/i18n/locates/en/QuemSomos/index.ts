ead49b0f58061d4b073dc5826425907ceb058838
Jonatan Cardoso <jonatancardosoc@gmail.com>
2023-08-30 01:25:36 -0300

    Tradução QuemSomos

--- src/i18n/locates/en/QuemSomos/index.ts
+++ src/i18n/locates/en/QuemSomos/index.ts
@@ -2,6 +2,6 @@ import { ResourceI18nType } from '@/i18n/locates/type'
 
 export const QuemSomosEN: ResourceI18nType['QUEM_SOMOS'] = {
   TITLE: 'WHO ARE WE?',
-  TEXT1: 'We are a team passionate and committed to the revolution of modern agriculture. Our focus is on precision agriculture, an approach that uses cutting-edge technology to transform the way we grow and harvest our food. We develop innovative technological solutions that combine sensors, data analysis, drones, and GPS systems to provide detailed information about the soil, crops, and weather conditions',
-  TEXT2: 'Our technology allows farmers to make informed decisions in real-time, and we are committed to improving the efficiency and sustainability of agriculture. Through the optimization of farming practices, we aim to maximize crop yields, reduce the waste of natural resources such as water and fertilizers, and promote environmental conservation.',
+  TEXT1: 'We are a platform focused on the evolution of modern agriculture. Our emphasis is on Precision Agriculture, with a set of technological tools that enable greater understanding by obtaining information about the cultivation area, allowing for higher yields in the daily activities of the field. We develop innovative technological solutions that combine tasks with the latest technologies such as sensors, data analysis, drones, and GPS systems to provide detailed information about the soil, crops, and weather conditions.',
+  TEXT2: 'Our technology allows users to make informed decisions in real-time, and we are committed to improving the efficiency and sustainability of agriculture. Through the optimization of farming practices, we aim to maximize crop yields, making agribusiness sustainable.',
 }
