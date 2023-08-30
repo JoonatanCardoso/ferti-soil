395b0e0a87064e27ac9bbbff53ab59b32a2452ae
Jonatan Cardoso <jonatancardosoc@gmail.com>
2023-08-30 01:25:06 -0300

    Tradução Treinamento

---
+++ src/i18n/locates/en/Treinamento/index.ts
@@ -0,0 +1,14 @@
+import { ResourceI18nType } from '@/i18n/locates/type'
+
+export const TreinamentoEN: ResourceI18nType['TREINAMENTO'] = {
+  TITLE: 'Our precision agriculture training!',
+  SUB_TITLE: 'For those who acquire the system, we offer comprehensive and personalized training, led by the Ferti Soil team. This ensures you are prepared to make the most of our precision agriculture platform, empowering you to make informed decisions and optimize the outcomes of your farming operation.',
+  TITLE1: 'Acquisition and Preparation',
+  TITLE2: 'Introduction to the Platform',
+  TITLE3: 'Use of Tools',
+  TITLE4: 'Problem Solving and Support',
+  TEXT1: 'After acquisition, we schedule training according to your needs.',
+  TEXT2: 'Familiarization with the interface and functionalities.',
+  TEXT3: 'Exploration of features like drones and GPS for monitoring.',
+  TEXT4: 'Guidance on handling challenges and access to technical support.',
+}
---
+++ src/i18n/locates/pt/Treinamento/index.ts
@@ -0,0 +1,14 @@
+import { ResourceI18nType } from '@/i18n/locates/type'
+
+export const TreinamentoPT: ResourceI18nType['TREINAMENTO'] = {
+  TITLE: 'Nosso treinamento sobre agricultura de precisão!',
+  SUB_TITLE: 'Para os adquirentes do sistema, oferecemos um treinamento completo e personalizado, liderado pela equipe Ferti Soil. Isso assegura que você esteja preparado para aproveitar ao máximo nossa plataforma de agricultura de precisão, capacitando-o a tomar decisões informadas e otimizar os resultados de sua operação agrícola.',
+  TITLE1: 'Aquisição e Preparação',
+  TITLE2: 'Introdução à Plataforma',
+  TITLE3: 'Utilização das Ferramentas',
+  TITLE4: 'Solução de Problemas e Suporte',
+  TEXT1: 'Após a aquisição, agendamos o treinamento de acordo com suas necessidades.',
+  TEXT2: 'Familiarização com a interface e funcionalidades.',
+  TEXT3: 'Exploração de recursos como dronese GPS para monitoramento.',
+  TEXT4: 'Orientação para lidar com desafios e acesso ao suporte técnico.',
+}
