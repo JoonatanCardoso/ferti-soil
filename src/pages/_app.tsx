59c628b0c264c2fd633f60d312af3c523a84569f
Jonatan Cardoso <jonatancardosoc@gmail.com>
2023-08-30 22:13:49 -0300

    Modificações na pagina

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,26 @@
+import Button from '@/components/Button'
 import { Layout } from '@/layouts'
 import { appWithTranslation } from 'next-i18next'
 import { NextSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import 'src/i18n'
+import '../app/globals.css'
 import nextSeo from '../config/seo'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Head>
-      <title>Ferti Soil</title>
-      <meta name='viewport' content='width=device-width,height=device-height initial-scale=1' />
-    </Head>
-    <Layout>
-      <NextSeo {...nextSeo} />
-      <Component {...pageProps} />
-    </Layout></>
+      <Head>
+        <title>Ferti Soil</title>
+        <meta name='viewport' content='width=device-width,height=device-height initial-scale=1' />
+      </Head>
+      <Layout>
+        <NextSeo {...nextSeo} />
+        <Component {...pageProps} />
+        <Button />
+      </Layout>
+    </>
   )
 }
 
