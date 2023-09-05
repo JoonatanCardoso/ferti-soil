1b6854746aaca5cc536a4327bc00d8ea706f01c9
Jonatan Cardoso <jonatancardosoc@gmail.com>
2023-09-05 01:07:05 -0300

    Alteração de texto

--- src/i18n/locates/en/QuemSomos/index.ts
+++ src/i18n/locates/en/QuemSomos/index.ts
@@ -4,6 +4,6 @@ export const QuemSomosEN: ResourceI18nType['QUEM_SOMOS'] = {
   TITLE: 'WHO ARE WE?',
   TEXT1: 'We are a solution aimed at Soil Fertility Management, pursuing modern and sustainable agriculture. Our focus is on Precision Agriculture, utilizing a suite of technological tools that enable enhanced understanding by gathering detailed information about every section of the cultivation area, thus enabling higher productivity in day-to-day field operations. We have developed innovative technological solutions that integrate best field practices with advanced technologies like sensors, data analysis, drones, and GPS systems to offer comprehensive insights into soil, crops, and weather conditions.',
   TEXT2: 'Our technology enables users to make informed decisions in real-time, and we are committed to enhancing the efficiency and sustainability of agriculture. Through the optimization of farming practices, we aim to maximize crop yields, making agribusiness sustainable.',
-  TEXT: 'Fertisoil: From Sampling to Recommendation, Recommending the Success of Your Land.',
+  TEXT: 'FertiSoil: From Sampling to Application. Recommending the success of your crop.',
   SLOGAN: 'Transforming agriculture with advanced technology, Fertisoil is your ally for efficient soil sampling and accurate recommendations.',
 }
